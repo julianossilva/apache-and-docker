0719a70c5e2d91379490e0510f41dd604db00a9d
Juliano S. Silva <julianosoaresdasilva@outlook.com>
2022-04-20 15:07:52 -0300

    change server port on run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker build -t my-apache2 .
-docker run -v $(pwd)/public:/usr/local/apache2/htdocs -p 80:80 my-apache2
+docker run -v $(pwd)/public:/usr/local/apache2/htdocs -p 8080:80 my-apache2
 
